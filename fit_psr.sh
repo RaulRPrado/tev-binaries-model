bd93314bf135b4b5ba4e6f91827c2814707aae45
RaulRPrado <raul.prado@usp.br>
2020-06-12 11:26:38 +0200

    adding scripts

---
+++ fit_psr.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+source "/afs/ifh.de/group/cta/scratch/prado/set_python3.sh"
+
+python3 fit_pulsar_model.py $1
---
+++ sub.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+for size in '' '_small'
+do
+    for orb in 'ca' 'mo'
+    do
+        label="${orb}${size}"
+        echo $label
+        qsub -cwd -l s_cpu=48:00:00 -N $label fit_psr.sh $label
+
+        # for par in 'm' 'i' 'e' 'o' 'p'
+        # do
+        #     for dir in 'sup' 'inf'
+        #     do
+        #         label="${orb}_${par}_${dir}${size}_test"
+        #         echo $label
+        #         qsub -cwd -l s_cpu=48:00:00 -N $label fit_psr.sh $label
+        #     done # dir
+        # done # par
+    done # orb
+done # size
