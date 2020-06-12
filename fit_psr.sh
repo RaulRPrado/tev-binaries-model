e8281e59ef5d8b885f9869601a298776045750c4
RaulRPrado <raul.prado@usp.br>
2020-06-12 11:33:44 +0200

    adding set_tgb

--- fit_psr.sh
+++ fit_psr.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 source "/afs/ifh.de/group/cta/scratch/prado/set_python3.sh"
+source "/afs/ifh.de/group/cta/scratch/prado/VTS/HESS_J0632/tev-binaries-model/set_tgb-lib.sh"
 
 python3 applications/fit_pulsar_model.py $1
